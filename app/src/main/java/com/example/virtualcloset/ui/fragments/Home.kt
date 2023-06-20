35e7907bc04a52b5d6fabc4b98efd196016c700d
AlexandraCarabas <81260155+AlexandraCarabas@users.noreply.github.com>
2023-06-21 01:43:10 +0300

    Implemented display fashion news on home page

--- app/src/main/java/com/example/virtualcloset/ui/fragments/Home.kt
+++ app/src/main/java/com/example/virtualcloset/ui/fragments/Home.kt
@@ -3,6 +3,7 @@ package com.example.virtualcloset.ui.fragments
 
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -10,8 +11,17 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import androidx.browser.customtabs.CustomTabsIntent
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import com.example.virtualcloset.databinding.FragmentHomeBinding
 import com.example.virtualcloset.R
+import com.example.virtualcloset.models.News
+import com.example.virtualcloset.ui.RecyclerViewAdapterNews
 import com.example.virtualcloset.ui.activities.SignInActivity
 import com.example.virtualcloset.utils.Constants
 import com.google.firebase.auth.FirebaseAuth
@@ -26,7 +36,7 @@ private const val ARG_PARAM2 = "param2"
  * Use the [Home.newInstance] factory method to
  * create an instance of this fragment.
  */
-class Home : Fragment() {
+class Home : Fragment(), RecyclerViewAdapterNews.OnNewsClick{
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,6 +44,8 @@ class Home : Fragment() {
 
     private lateinit var binding: FragmentHomeBinding
     private lateinit var myTextView: TextView
+    private lateinit var myAdaptor: RecyclerViewAdapterNews
+    private lateinit var newsArrayList: ArrayList<News>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -64,7 +76,50 @@ class Home : Fragment() {
             activity?.startActivity(Intent(this.activity,SignInActivity::class.java))
             activity?.finish()
         }
+
+        val recyclerView : RecyclerView = view.findViewById(R.id.recyclerViewNews)
+        recyclerView.layoutManager = LinearLayoutManager(this@Home.requireContext())
+
+        myAdaptor = RecyclerViewAdapterNews(this)
+        recyclerView.adapter = myAdaptor
+
+        fetchNews()
+
         return view
+    }
+
+
+
+    fun fetchNews() {
+        val queue = Volley.newRequestQueue(this@Home.requireContext())
+        val url = "https://newsapi.ai/api/v1/article/getArticles?query=%7B%22%24query%22%3A%7B%22%24and%22%3A%5B%7B%22conceptUri%22%3A%22http%3A%2F%2Fen.wikipedia.org%2Fwiki%2FFashion%22%7D%2C%7B%22categoryUri%22%3A%22dmoz%2FShopping%2FClothing%22%7D%5D%7D%2C%22%24filter%22%3A%7B%22forceMaxDataTimeWindow%22%3A%2231%22%7D%7D&resultType=articles&articlesSortBy=date&articlesCount=10&articleBodyLen=-1&apiKey=d03f661e-a943-4fe1-bdb1-d0cc65a7a848"
+        val jsonObjectRequest = JsonObjectRequest(
+            Request.Method.GET, url, null,
+            Response.Listener {
+                val newsJsonArray = it.getJSONObject("articles").getJSONArray("results")
+                val newsArray = ArrayList<News>()
+                for(i in 0 until newsJsonArray.length()){
+                    val newsJsonObject = newsJsonArray.getJSONObject(i)
+                    val news = News (
+                        newsJsonObject.getString("title"),
+                        newsJsonObject.getString("url"),
+                        newsJsonObject.getString("image")
+                    )
+                    newsArray.add(news)
+                }
+                myAdaptor.updateData(newsArray)
+            },
+            Response.ErrorListener {
+
+            }
+        )
+        queue.add(jsonObjectRequest)
+    }
+
+    override fun onClicked(news: News) {
+        val builder = CustomTabsIntent.Builder()
+        val customTabsIntent = builder.build()
+        customTabsIntent.launchUrl(this@Home.requireContext(), Uri.parse(news.url))
     }
 
     companion object {
