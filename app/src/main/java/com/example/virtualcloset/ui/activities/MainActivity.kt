843fac83aa59526ffd9e7d3089c12b2fc282d8cb
AlexandraCarabas <81260155+AlexandraCarabas@users.noreply.github.com>
2023-06-21 18:20:18 +0300

    Set Splash Screen and app icon

--- app/src/main/java/com/example/virtualcloset/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/virtualcloset/ui/activities/MainActivity.kt
@@ -4,11 +4,14 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.window.SplashScreen
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.virtualcloset.R
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
